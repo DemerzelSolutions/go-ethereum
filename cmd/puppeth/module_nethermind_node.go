46c0a314a46513466195a45897fc4bde7352b5ed
Tomasz Kajetan Stanczak <tkstanczak@users.noreply.github.com>
2019-01-01 20:52:15 +0000

    adding Nethermind node deployment - WIP

---
+++ cmd/puppeth/module_nethermind_node.go
@@ -0,0 +1,145 @@
+// Copyright 2017 The go-ethereum Authors
+// This file is part of go-ethereum.
+//
+// go-ethereum is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// go-ethereum is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with go-ethereum. If not, see <http://www.gnu.org/licenses/>.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"path/filepath"
+	"strings"
+	"text/template"
+)
+
+// nodeDockerfile is the Dockerfile required to run an Ethereum node.
+var nethermindNodeDockerfile = `
+FROM ethereum/client-go:latest
+
+ADD genesis.json /genesis.json
+{{if .Unlock}}
+	ADD signer.json /signer.json
+	ADD signer.pass /signer.pass
+{{end}}
+RUN \
+  echo 'geth --cache 512 init /genesis.json' > geth.sh && \{{if .Unlock}}
+	echo 'mkdir -p /root/.ethereum/keystore/ && cp /signer.json /root/.ethereum/keystore/' >> geth.sh && \{{end}}
+	echo $'exec geth --networkid {{.NetworkID}} --cache 512 --port {{.Port}} --nat extip:{{.IP}} --maxpeers {{.Peers}} {{.LightFlag}} --ethstats \'{{.Ethstats}}\' {{if .Bootnodes}}--bootnodes {{.Bootnodes}}{{end}} {{if .Etherbase}}--miner.etherbase {{.Etherbase}} --mine --miner.threads 1{{end}} {{if .Unlock}}--unlock 0 --password /signer.pass --mine{{end}} --miner.gastarget {{.GasTarget}} --miner.gaslimit {{.GasLimit}} --miner.gasprice {{.GasPrice}}' >> geth.sh
+
+ENTRYPOINT ["/bin/sh", "geth.sh"]
+`
+
+// nodeComposefile is the docker-compose.yml file required to deploy and maintain
+// an Ethereum node (bootnode or miner for now).
+var nethermindNodeComposefile = `
+version: '2'
+services:
+  {{.Type}}:
+    build: .
+    image: {{.Network}}/{{.Type}}
+    container_name: {{.Network}}_{{.Type}}_1
+    ports:
+      - "{{.Port}}:{{.Port}}"
+      - "{{.Port}}:{{.Port}}/udp"
+    volumes:
+      - {{.Datadir}}:/root/.ethereum{{if .Ethashdir}}
+      - {{.Ethashdir}}:/root/.ethash{{end}}
+    environment:
+      - PORT={{.Port}}/tcp
+      - TOTAL_PEERS={{.TotalPeers}}
+      - LIGHT_PEERS={{.LightPeers}}
+      - STATS_NAME={{.Ethstats}}
+      - MINER_NAME={{.Etherbase}}
+      - GAS_TARGET={{.GasTarget}}
+      - GAS_LIMIT={{.GasLimit}}
+      - GAS_PRICE={{.GasPrice}}
+    logging:
+      driver: "json-file"
+      options:
+        max-size: "1m"
+        max-file: "10"
+    restart: always
+`
+
+// deployNode deploys a new Ethereum node container to a remote machine via SSH,
+// docker and docker-compose. If an instance with the specified network name
+// already exists there, it will be overwritten!
+func deployNethermindNode(client *sshClient, network string, bootnodes []string, config *nodeInfos, nocache bool) ([]byte, error) {
+	kind := "sealnode"
+	if config.keyJSON == "" && config.etherbase == "" {
+		kind = "bootnode"
+		bootnodes = make([]string, 0)
+	}
+	// Generate the content to upload to the server
+	workdir := fmt.Sprintf("%d", rand.Int63())
+	files := make(map[string][]byte)
+
+	lightFlag := ""
+	if config.peersLight > 0 {
+		lightFlag = fmt.Sprintf("--lightpeers=%d --lightserv=50", config.peersLight)
+	}
+	dockerfile := new(bytes.Buffer)
+	template.Must(template.New("").Parse(nodeDockerfile)).Execute(dockerfile, map[string]interface{}{
+		"NetworkID": config.network,
+		"Port":      config.port,
+		"IP":        client.address,
+		"Peers":     config.peersTotal,
+		"LightFlag": lightFlag,
+		"Bootnodes": strings.Join(bootnodes, ","),
+		"Ethstats":  config.ethstats,
+		"Etherbase": config.etherbase,
+		"GasTarget": uint64(1000000 * config.gasTarget),
+		"GasLimit":  uint64(1000000 * config.gasLimit),
+		"GasPrice":  uint64(1000000000 * config.gasPrice),
+		"Unlock":    config.keyJSON != "",
+	})
+	files[filepath.Join(workdir, "Dockerfile")] = dockerfile.Bytes()
+
+	composefile := new(bytes.Buffer)
+	template.Must(template.New("").Parse(nodeComposefile)).Execute(composefile, map[string]interface{}{
+		"Type":       kind,
+		"Datadir":    config.datadir,
+		"Ethashdir":  config.ethashdir,
+		"Network":    network,
+		"Port":       config.port,
+		"TotalPeers": config.peersTotal,
+		"Light":      config.peersLight > 0,
+		"LightPeers": config.peersLight,
+		"Ethstats":   config.ethstats[:strings.Index(config.ethstats, ":")],
+		"Etherbase":  config.etherbase,
+		"GasTarget":  config.gasTarget,
+		"GasLimit":   config.gasLimit,
+		"GasPrice":   config.gasPrice,
+	})
+	files[filepath.Join(workdir, "docker-compose.yaml")] = composefile.Bytes()
+
+	files[filepath.Join(workdir, "genesis.json")] = config.genesis
+	if config.keyJSON != "" {
+		files[filepath.Join(workdir, "signer.json")] = []byte(config.keyJSON)
+		files[filepath.Join(workdir, "signer.pass")] = []byte(config.keyPass)
+	}
+	// Upload the deployment files to the remote server (and clean up afterwards)
+	if out, err := client.Upload(files); err != nil {
+		return out, err
+	}
+	defer client.Run("rm -rf " + workdir)
+
+	// Build and deploy the boot or seal node service
+	if nocache {
+		return nil, client.Stream(fmt.Sprintf("cd %s && docker-compose -p %s build --pull --no-cache && docker-compose -p %s up -d --force-recreate --timeout 60", workdir, network, network))
+	}
+	return nil, client.Stream(fmt.Sprintf("cd %s && docker-compose -p %s up -d --build --force-recreate --timeout 60", workdir, network))
+}
